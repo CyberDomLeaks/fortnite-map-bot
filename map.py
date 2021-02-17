067deeae0b924704eee408be9ca08a79f0c3c2dd
Cyber Dom <65227037+CyberDomLeaks@users.noreply.github.com>
2021-02-17 07:03:33 -0500

    very small changes, making sure it still works

--- map.py
+++ map.py
@@ -5,17 +5,17 @@ import time
 from PIL import Image
 from colorama import *
 init()
-print("Starting program...")
+print("Starting..")
 url = 'https://fortnite-api.com/images/map_en.png'
 r = requests.get(url, allow_redirects=True)
 open('map.png', 'wb').write(r.content)
 response = requests.get('https://benbotfn.tk/api/v1/status')
 version = response.json()['currentFortniteVersionNumber']
-print(Fore.BLUE+"Retrived The Map, Saving..")
+print(Fore.BLUE+"Retrived The Map, Saving image..")
 img=Image.open('map.png')
 img=img.resize((1200,1200),PIL.Image.ANTIALIAS)
 img.save('smallmap.png')
 os.remove('map.png')
-print(Fore.BLUE+"saved the map")
-print("Finished program. Closing in 5 seconds.")
-time.sleep(5)
+print(Fore.BLUE+"Saved map!")
+print("Finished,losing in 2 seconds.")
+time.sleep(2)
